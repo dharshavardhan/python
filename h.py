3a0bfd420c350898176051fceb8ddf8c36ec71ca
harsha <hvdevarasetti@gmail.com>
2021-04-27 22:46:17 +0530

    new commit

---
+++ h.py
@@ -0,0 +1,35 @@
+a=input("please enter the first number")
+b=input("please enter the second number")
+a=int(a)
+b=int(b)
+print(a+b)
+a=input("please enter the number")
+b=input("please enter the number")
+a=int(a)
+b=int(b)
+print(a-b)
+a=input("please enter the first number")
+b=input("please enter the second number")
+a=int(a)
+b=int(b)
+print(a*b)
+a=input("please enter the first number")
+b=input("please enter the second number")
+a=int(a)
+b=int(b)
+print(a/b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ h1.py
@@ -0,0 +1,19 @@
+a=input("enter the first number")
+b=input("enter the second number")
+a=int(a)
+b=int(b)
+print(a**2+b**2+2*a*b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
